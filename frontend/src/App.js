0ae47c13a643af8ad6dbdbd67c08894fe9471f56
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-14 07:35:09 -0500

    React-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import { Container } from 'react-bootstrap';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
