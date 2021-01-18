d91becadfdca65c0552633cfc83b8e5a7e45fd07
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-18 06:26:02 -0500

    Cart Screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -18,8 +18,12 @@ const CartScreen = ({ match, location, history }) => {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
+
   const removeFromCartHandler = (id) => {
     return 'remove';
+  }
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
   }
   return (
     <Row>
@@ -60,6 +64,10 @@ const CartScreen = ({ match, location, history }) => {
           <ListGroup variant='flush'>
            <ListGroup.Item>
              <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+             ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+           </ListGroup.Item>
+           <ListGroup.Item>
+             <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>Proceed to Checkout</Button>
            </ListGroup.Item>
           </ListGroup>
         </Card>
