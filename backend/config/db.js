961b5b74591f0dee7b96ea199dc2b33b42343326
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-23 19:00:18 -0500

    Show User In Navbar & Logout

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async() => {
       useNewUrlParser: true,
       useCreateIndex: true,
     })
-    console.log(`MongoDB Connected. ${conn.connection.host}`.yellow.inverse)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.inverse)
   } catch(error){ 
     console.error(`Error: ${error.message}`.red.inverse);
     process.exit(1)
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from './../constants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from './../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -15,19 +15,24 @@ export const login = (email, password) => async (dispatch) => {
       email: email,
       password: password
     }
-    console.log(body);
+    // console.log(body);
     const { data } = await axios.post('/api/users/login', body, config );
-    console.log(data);
+    // console.log(data);
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data
     })
     localStorage.setItem('userInfo', JSON.stringify(data))
   } catch(error){
-    console.log(error.response)
+    // console.log(error.response)
     dispatch({ 
       type: USER_LOGIN_FAIL, 
       payload: error.response && error.response.data.message ? error.response.data.message : error.message
     })
   }
+}
+
+export const logout = () => async dispatch => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 }
