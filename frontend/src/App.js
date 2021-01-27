1554220f026078529719bf69646becc59bb92b41
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-27 09:36:01 -0500

    Admin product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import ProductListScreen from './screens/ProductListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/' exact component={HomeScreen} />
         </Container>
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from './../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
+  console.log('inside reducer')
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
     const { data } = await axios.get('/api/products');
