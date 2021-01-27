08ee52f059aeb82c4ea4ba3e228eb214c777f84e
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-27 10:51:05 -0500

    Admin Delete Products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const Product = require('../models/productModel');
 const router = express.Router();
+const { protect, admin } = require('./../middlewares/authMiddleware');
 const { getProducts, getProductById } = require('./../controllers/productController');
 
 router.get('/', async (req, res) => {
@@ -18,6 +20,23 @@ router.get('/:id', async (req, res) => {
     const { params } = req;
     const product = await getProductById(params.id);
     res.json(product);
+  } catch(error){
+    console.log(error);
+    res.status(404).json({ message: 'Product not found' });
+  }
+})
+
+// Delete a product
+router.delete('/:id', protect, admin, async (req, res) => {
+  try {
+    const { params } = req;
+    const product = await Product.findById(params.id);
+    if(product){
+      await product.remove();
+      res.json({ message: 'Product removed'});
+    } else {
+      throw new Error()
+    }
   } catch(error){
     console.log(error);
     res.status(404).json({ message: 'Product not found' });
