b2f0156b509fa6e6e8194f6963d0e2014da2008e
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-27 16:52:25 -0500

    Image upload Config and Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -8,6 +9,8 @@ const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');  
+const { updateProduct } = require('../frontend/src/actions/productActions');
 
 dotenv.config();
 connectDB();
@@ -29,8 +32,11 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 // paypal config route
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 const PORT = process.env.PORT || 5000;
 
