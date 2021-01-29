bcafcee91b5315acc1c29b09fd147773b7b72063
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-01-28 19:10:18 -0500

    Custom Page Titles & Meta

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 // import axios from 'axios';
@@ -9,6 +10,7 @@ import Message from './../components/Message';
 import Loader from './../components/Loader';
 import Paginate from './../components/Paginate';
 import ProductCarousel from './../components/ProductCarousel';
+import Meta from './../components/Meta';
 
 const HomeScreen = ({ match }) => {
   // const [products, setProducts] = useState([]);
@@ -31,7 +33,8 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
-    {!keyword && <ProductCarousel />}
+    <Meta />
+    {!keyword ? <ProductCarousel /> : (<Link to='/' className='btn btn-light'>Go Back</Link>)}
       <h2>Latest Products</h2>
       { loading ? (
         <Loader />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,6 +7,7 @@ import Rating from './../components/Rating';
 // import products from './../products';
 import  Loader from './../components/Loader';
 import Message from './../components/Message';
+import Meta from './../components/Meta';
 import { listProductDetails, createProductReview } from '../actions/productActions';
 import { PRODUCT_CREATE_REVIEW_RESET } from './../constants/productConstants';
 
@@ -70,6 +71,7 @@ const ProductScreen = ( props ) => {
         <Message variant='danger'>{error}</Message> 
       ) : (
         <>
+        <Meta title={product.name}/>
         <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
